355644ff7ca1ccca015c3ad1718280d61cbe2ba3
gerardl <tad284@gmail.com>
2016-11-06 23:59:02 -0600

    make test more interesting

--- Assets/PoolingSystem/Scrips/Demo/CreatePoolsFromCode.cs
+++ Assets/PoolingSystem/Scrips/Demo/CreatePoolsFromCode.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using PoolingSystem.Pooling;
 
 namespace PoolingSystem.Demo
@@ -9,7 +9,8 @@ namespace PoolingSystem.Demo
 
         public GameObject objectToPool;
         public string poolName = "TestPrefab1";
-        private GameObject objectToReturn;
+        private List<GameObject> objectsToReturn;
+
 
         public GameObject fireballObjectToPool;
         public string fireballPoolName = "Fireball";
@@ -17,7 +18,7 @@ namespace PoolingSystem.Demo
         private void Start()
         {
             // Create a pool, this also returns the pool so we can save a reference
-            PoolingManager.CreatePoolingList(objectToPool, poolName, 100);
+            PoolingManager.CreatePoolingList(objectToPool, poolName, 1000);
             // but lets make sure that getting it again works
             PoolingList testPrefabPool = PoolingManager.GetPoolByName(poolName);
             // now get an object directly from our pooled list. I left methods here because I 
@@ -33,18 +34,24 @@ namespace PoolingSystem.Demo
             }
 
             // but for most cases you would probably just want to grab an object easily from the manager by name
-            objectToReturn = PoolingManager.GetPooledObjectByName(poolName);
-            if (objectToReturn != null)
+            objectsToReturn = new List<GameObject>();
+            for (int i = 0; i < 100; i++)
             {
-                Debug.Log("Got Prefab directly from PoolingManager: " + testPrefabPool.name);
-                // manipulate it a bit
-                objectToReturn.transform.position = new Vector3(Mathf.PingPong(Time.time, 8) - 4, transform.position.y, transform.position.z);
-                // now lets return an object to the pool after a delay.
-                // The code to return an object lives on the object itself in
-                // a class called PooledObject.  This is added to each object
-                // in the pool during pool creation
-                Invoke("ReturnToPoolingList", 5);
+                var newObject = PoolingManager.GetPooledObjectByName(poolName);
+                if (newObject != null)
+                {
+                    objectsToReturn.Add(newObject);
+                    Debug.Log("Got Prefab directly from PoolingManager: " + testPrefabPool.name);
+                    // manipulate it a bit
+                    newObject.transform.position = new Vector3(Mathf.PingPong(Time.time, 8) - 4, transform.position.y, transform.position.z);
+                    
+                }
             }
+            // now lets return an object to the pool after a delay.
+            // The code to return an object lives on the object itself in
+            // a class called PooledObject.  This is added to each object
+            // in the pool during pool creation
+            Invoke("ReturnToPoolingList", 5);
 
             // create a new fireball pool, get an object from it
             // and then use a user created script on that object
@@ -53,14 +60,14 @@ namespace PoolingSystem.Demo
 
         private void ReturnToPoolingList()
         {
-            Debug.Log("Returning object to pool");
-            objectToReturn.SetActive(false);
+            Debug.Log("Returning objects to pool");
+            objectsToReturn.ForEach(f => f.SetActive(false));
         }
 
         public void CreateFireballPool()
         {
-            // create a new pool, start it at 0 to force to it to grow only when we need it
-            PoolingManager.CreatePoolingList(fireballObjectToPool, fireballPoolName, 0);
+            // create a new pool
+            PoolingManager.CreatePoolingList(fireballObjectToPool, fireballPoolName, 10);
             // get a fireball and use it
             var fireball = PoolingManager.GetPooledObjectByName(fireballPoolName);
             if (fireball != null) fireball.GetComponent<Fireball>().Fire();
